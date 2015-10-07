2351c1bd839e12905b056e3487b00d608fa6294a
David Sklar <dfsklar@gmail.com>
2015-10-06 20:59:18 -0700

    Test of the timespec-did-change reporting looks good

--- SoundEditor/ViewController.swift
+++ SoundEditor/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MessagesFromTimelineDelegate {
     
     
     @IBOutlet weak var timeline: View_Timeline!
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
         
         try! timeline.createSoundbite("Intro", channelIndex:0, spec: Timespec(start:0, end:3))
         try! timeline.createSoundbite("Main", channelIndex:1,  spec: Timespec(start:3, end:7.8))
+        
+        timeline.registerDelegate(self)
         
         
         // *** THIS WOULD BE HOW I WOULD DO PROGRAMMATIC CREATION OF CUSTVIEW CHILDREN.
@@ -36,6 +38,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    
+    func soundbiteTimespecDidChange(name:String, newSpec:Timespec) {
+        
+    }
+    
+    func soundbiteDeleteRequested(name:String) {
+    
+    }
+    
+    func soundbiteDuplicateRequested(name:String) {
+        
+    }
+    
+    func soundbiteRenameRequested(nameCurrent:String, nameNew:String) {
+        
+    }
+
 
 
 }
